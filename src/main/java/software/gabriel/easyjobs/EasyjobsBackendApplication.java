bbe0d0922f87c998d2408fedc27094a5726109a9
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-06 20:06:40 -0300

    Commit inicial

---
+++ src/main/java/software/gabriel/easyjobs/EasyjobsBackendApplication.java
@@ -0,0 +1,13 @@
+package software.gabriel.easyjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyjobsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyjobsBackendApplication.class, args);
+	}
+
+}
