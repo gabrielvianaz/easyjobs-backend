b222315d78b797efc296ea3973108a12a9c1fa70
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-06 21:29:06 -0300

    Criada estrutura básica de tratamento de exceções

---
+++ src/main/java/software/gabriel/easyjobs/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package software.gabriel.easyjobs.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ *
+ * @author gabriel
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
