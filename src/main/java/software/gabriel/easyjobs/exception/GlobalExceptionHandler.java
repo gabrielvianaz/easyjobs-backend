460ca16306f91b117871e3d1758400c57df4802d
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-06 21:54:18 -0300

    Inserido ExceptionHandler para MethodArgumentNotValidException

--- src/main/java/software/gabriel/easyjobs/exception/GlobalExceptionHandler.java
+++ src/main/java/software/gabriel/easyjobs/exception/GlobalExceptionHandler.java
@@ -4,7 +4,13 @@
  */
 package software.gabriel.easyjobs.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  *
@@ -12,5 +18,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String mensagem = e.getFieldError().getDefaultMessage();
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        BaseError err = new BaseError(Instant.now(), status.value(), mensagem, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
