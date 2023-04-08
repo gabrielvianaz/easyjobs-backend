7860eccfccb1ff16027681cbc508d2ee6d496c12
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-08 17:54:30 -0300

    Criada classe AtivacaoUtils para armazenar definições do código de ativação

---
+++ src/main/java/software/gabriel/easyjobs/utils/AtivacaoUtils.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package software.gabriel.easyjobs.utils;
+
+/**
+ *
+ * @author gabriel
+ */
+public class AtivacaoUtils {
+
+    public static Integer TAMANHO_CODIGO_ATIVACAO = 6;
+
+    public static Integer DIAS_EXPIRACAO_CODIGO_ATIVACAO = 1;
+
+    public static Integer MINUTOS_RENOVACAO_CODIGO_ATIVACAO = 10;
+
+}
