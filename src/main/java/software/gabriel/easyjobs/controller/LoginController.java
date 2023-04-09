ccc36689e11dca3276d137e37105bc595afa4d35
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-09 12:32:14 -0300

    Utilização do método addCookie da HttpServletResponse para adicionar cookie à ResponseEntity

--- src/main/java/software/gabriel/easyjobs/controller/LoginController.java
+++ src/main/java/software/gabriel/easyjobs/controller/LoginController.java
@@ -5,15 +5,16 @@
 package software.gabriel.easyjobs.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.gabriel.easyjobs.dto.UsuarioDTO;
+import software.gabriel.easyjobs.security.SecurityUtils;
 import software.gabriel.easyjobs.service.LoginService;
 
 /**
@@ -28,25 +29,20 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping
-    public ResponseEntity<Void> login(@RequestBody @Valid UsuarioDTO usuarioDTO) {
+    public ResponseEntity<Void> login(@RequestBody @Valid UsuarioDTO usuarioDTO, HttpServletResponse response) {
         String token = loginService.getToken(usuarioDTO);
-        return ResponseEntity.ok().headers(getHeader(token)).build();
+        Cookie cookie = getCookie(token);
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
     }
 
     private Cookie getCookie(String token) {
         Cookie cookie = new Cookie("jwt", token);
         cookie.setHttpOnly(true);
-        cookie.setMaxAge(86400);
+        cookie.setMaxAge(SecurityUtils.SEGUNDOS_EXPIRACAO_TOKEN_JWT);
         cookie.setPath("/");
         cookie.setSecure(true);
         return cookie;
-    }
-
-    private HttpHeaders getHeader(String token) {
-        Cookie cookie = getCookie(token);
-        HttpHeaders header = new HttpHeaders();
-        header.add(HttpHeaders.SET_COOKIE, cookie.getName() + "=" + cookie.getValue());
-        return header;
     }
 
 }
