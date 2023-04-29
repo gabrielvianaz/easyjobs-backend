82f8216f9735e4748be93c20f306163307fb52ae
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 17:49:29 -0300

    Classe renomeada e movida para o pacote mapper.base

--- src/main/java/software/gabriel/easyjobs/mapper/base/DefaultMapper.java
+++ src/main/java/software/gabriel/easyjobs/mapper/base/DefaultMapper.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package software.gabriel.easyjobs.mapper;
+package software.gabriel.easyjobs.mapper.base;
 
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
-import software.gabriel.easyjobs.dto.BaseDTO;
-import software.gabriel.easyjobs.entity.BaseEntity;
+import software.gabriel.easyjobs.dto.base.AbstractDTO;
+import software.gabriel.easyjobs.entity.base.AbstractEntity;
 
 /**
  *
  * @author gabriel
  */
 @MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface BaseMapper<T extends BaseEntity, R extends BaseDTO<T>> {
+public interface DefaultMapper<T extends AbstractEntity, R extends AbstractDTO<T>> {
 
     T toEntity(R dto);
 
