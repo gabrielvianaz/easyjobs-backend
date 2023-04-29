1d84cb92f6786c06045c7aebecf48c6f6738820e
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 13:02:33 -0300

    Classes estendendo AbstractController e movidas para pacotes próprios

--- src/main/java/software/gabriel/easyjobs/controller/candidato/CandidatoController.java
+++ src/main/java/software/gabriel/easyjobs/controller/candidato/CandidatoController.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package software.gabriel.easyjobs.controller;
+package software.gabriel.easyjobs.controller.candidato;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.gabriel.easyjobs.dto.CandidatoDTO;
-import software.gabriel.easyjobs.service.CandidatoService;
+import software.gabriel.easyjobs.controller.base.AbstractController;
+import software.gabriel.easyjobs.dto.candidato.CandidatoDTO;
+import software.gabriel.easyjobs.service.candidato.CandidatoService;
 
 /**
  *
@@ -21,14 +21,12 @@ import software.gabriel.easyjobs.service.CandidatoService;
  */
 @RestController
 @RequestMapping("/candidatos")
-public class CandidatoController {
-
-    @Autowired
-    CandidatoService candidatoService;
+public class CandidatoController
+        extends AbstractController<CandidatoService> {
 
     @PostMapping
     public ResponseEntity<Void> cadastrar(@Valid @RequestBody CandidatoDTO candidatoDTO) {
-        candidatoService.cadastrar(candidatoDTO);
+        service.cadastrar(candidatoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
--- src/main/java/software/gabriel/easyjobs/controller/empresa/EmpresaController.java
+++ src/main/java/software/gabriel/easyjobs/controller/empresa/EmpresaController.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package software.gabriel.easyjobs.controller;
+package software.gabriel.easyjobs.controller.empresa;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.gabriel.easyjobs.dto.EmpresaDTO;
-import software.gabriel.easyjobs.service.EmpresaService;
+import software.gabriel.easyjobs.controller.base.AbstractController;
+import software.gabriel.easyjobs.dto.empresa.EmpresaDTO;
+import software.gabriel.easyjobs.service.empresa.EmpresaService;
 
 /**
  *
@@ -21,14 +21,12 @@ import software.gabriel.easyjobs.service.EmpresaService;
  */
 @RestController
 @RequestMapping("/empresas")
-public class EmpresaController {
-
-    @Autowired
-    EmpresaService empresaService;
+public class EmpresaController
+        extends AbstractController<EmpresaService> {
 
     @PostMapping
     public ResponseEntity<Void> cadastrar(@Valid @RequestBody EmpresaDTO empresaDTO) {
-        empresaService.cadastrar(empresaDTO);
+        service.cadastrar(empresaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
