e6bcbad13e680833c0af1b10c3cfd29d5ff9497d
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 13:08:56 -0300

    Classe renomeada e movida para o pacote dto.base

--- src/main/java/software/gabriel/easyjobs/dto/base/AbstractDTO.java
+++ src/main/java/software/gabriel/easyjobs/dto/base/AbstractDTO.java
@@ -2,15 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package software.gabriel.easyjobs.dto;
+package software.gabriel.easyjobs.dto.base;
 
 import java.io.Serializable;
+import software.gabriel.easyjobs.entity.base.AbstractEntity;
 
 /**
  *
  * @author gabriel
  */
-public class BaseDTO<T>
+public abstract class AbstractDTO<T extends AbstractEntity>
         implements Serializable {
 
     private static final long serialVersionUID = 1L;
