f42bf8743f4fa5eaa53f138cfd3031b98c8ae888
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 17:52:12 -0300

    Classe BaseModel renomeada e movida para o pacote model.base, NamedEntityModel implementada

--- src/main/java/software/gabriel/easyjobs/model/base/EntityModel.java
+++ src/main/java/software/gabriel/easyjobs/model/base/EntityModel.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package software.gabriel.easyjobs.model;
+package software.gabriel.easyjobs.model.base;
 
 /**
  *
  * @author gabriel
  */
-public interface BaseModel {
+public interface EntityModel {
     
     Long getId();
     
---
+++ src/main/java/software/gabriel/easyjobs/model/base/NamedEntityModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package software.gabriel.easyjobs.model.base;
+
+/**
+ *
+ * @author gabriel
+ */
+public interface NamedEntityModel 
+        extends EntityModel {
+
+    String getEntityName();
+
+}
