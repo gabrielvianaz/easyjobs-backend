26674bbac1170acfc74c08387d48ad58895b07e0
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 13:13:53 -0300

    Classes movidas para o pacote dto.security e removido atributo id do UsuarioDTO, visto que herda de AbstractDTO

--- src/main/java/software/gabriel/easyjobs/dto/security/AtivacaoDTO.java
+++ src/main/java/software/gabriel/easyjobs/dto/security/AtivacaoDTO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package software.gabriel.easyjobs.dto;
+package software.gabriel.easyjobs.dto.security;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
--- src/main/java/software/gabriel/easyjobs/dto/security/UsuarioDTO.java
+++ src/main/java/software/gabriel/easyjobs/dto/security/UsuarioDTO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package software.gabriel.easyjobs.dto;
+package software.gabriel.easyjobs.dto.security;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
@@ -10,17 +10,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
-import software.gabriel.easyjobs.entity.Role;
-import software.gabriel.easyjobs.entity.Usuario;
+import software.gabriel.easyjobs.dto.base.AbstractDTO;
+import software.gabriel.easyjobs.entity.security.Role;
+import software.gabriel.easyjobs.entity.security.Usuario;
 
 /**
  *
  * @author gabriel
  */
 public class UsuarioDTO
-        extends BaseDTO<Usuario> {
-
-    private Long id;
+        extends AbstractDTO<Usuario> {
 
     @NotBlank(message = "O campo 'email' deve ser preenchido!")
     @Email(message = "O e-mail informado é inválido!")
@@ -32,16 +31,6 @@ public class UsuarioDTO
     private String senha;
 
     private Set<Role> roles = new HashSet<>();
-
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getEmail() {
         return email;
