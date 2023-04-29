3e185f26cc060ca909ec3d1eb4bf788a9c5d3c81
Gabriel Viana <gabrielvianagvm@gmail.com>
2023-04-29 12:44:07 -0300

    Classe movida para o pacote Utils

---
+++ src/main/java/software/gabriel/easyjobs/utils/SecurityUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package software.gabriel.easyjobs.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.security.core.context.SecurityContextHolder;
+import software.gabriel.easyjobs.entity.security.Usuario;
+
+/**
+ *
+ * @author gabriel
+ */
+public class SecurityUtils {
+
+    public static String getJwtSecret() {
+        return System.getenv("JWT_SECRET");
+    }
+
+    public static List<String> getAllowedOrigins() {
+        return Arrays.asList("http://localhost:5173");
+    }
+
+    public static Usuario getUsuarioAutenticado() {
+        return (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public static Integer SEGUNDOS_EXPIRACAO_TOKEN_JWT = 86400;
+
+}
